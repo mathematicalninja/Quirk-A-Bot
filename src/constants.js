da88afdaea40720dc13fcc128e157e9c607a85fe
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-17 16:19:55 +0000

    dice rolling checks if any numbers have been defined, and rouse regex requires "rou" atleast

--- src/constants.js
+++ src/constants.js
@@ -4,10 +4,10 @@
 /**
  * matches Roll and it's easiest typos followed by two numbers, sperating all three parts by anything other than digits (e.g. roll532 makes no sense, but RoL5 2 does) Matches a one or two numbers, if the second number exists
  */
-export const DICE_ROLL_REGEX = /r+o+l+\D*(\d+)\D*(\d*)/i;
+export const DICE_ROLL_REGEX = /r+o+l+\D*(\d*)\D*(\d*)/i;
 
 // todo dont need to match "rouse" separately
 /**
- * matches if a player asks to ro[use], or ro[use] <n>.
+ * matches if a player asks to rou[se], or rou[se] <n>.
  */
-export const ROUSE_REGEX = /r+o+u*s*e*\D*(\d+)*/i;
+export const ROUSE_REGEX = /r+o+u+s*e*\D*(\d*)/i;
