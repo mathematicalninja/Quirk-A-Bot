2c5ebfa3e26e6d844b6c8f757c1eb595424ace7f
mathematicalninja <github@mathematics.ninja>
2021-02-17 00:28:11 +0000

    Changed newlining for regular rolls, to bring in line with new Rouse.

--- DiceRolling.js
+++ DiceRolling.js
@@ -126,11 +126,11 @@ function showRolls([cleanResults, hungryResults]) {
 	let hungryString = '';
 
 	//Add clean result text if there are any clean dice
-	if (cleanResults.length) cleanString = `Clean Dice: ${cleanResults.join(', ')}`;
+	if (cleanResults.length) cleanString = `\nClean Dice: ${cleanResults.join(', ')}`;
 
 	//Add hungry result text if there are any hungry dice
 	//if there are clean dice, newline before the Hungry dice
-	if (hungryResults.length) hungryString = `${cleanString ? '\n' : ''}Hungry Dice: ${hungryResults.join(', ')}`;
+	if (hungryResults.length) hungryString = `\nHungry Dice: ${hungryResults.join(', ')}`;
 
 	return cleanString + hungryString;
 };
