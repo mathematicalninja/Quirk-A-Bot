5140f9b4827277b69600b5aea1276d947790eaa1
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-17 16:45:35 +0000

    renamed DiceRolling to main as the bot will do more than rolling

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 /* eslint-disable import/no-unresolved, no-global-assign */
 require = require('esm')(module); // enable modern ES6 module import/export
-module.exports = require('./DiceRolling.js'); // start main program
+module.exports = require('./src/main.js'); // start main program
