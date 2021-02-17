c199842aa41862e19153a57486abcedfb7e0a677
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-17 16:53:42 +0000

    tweaked start and dev scripts to default to main file, and tweaked comments to show source file

--- src/discord-handlers/handleReady.js
+++ src/discord-handlers/handleReady.js
@@ -1,4 +1,4 @@
 /** Discord ready handler */
 export default function handleReady() {
-	console.log('loged on ok'); //the typo is intentional.
+	console.log(__filename, '\n\tloged on ok'); //the typo is intentional.
 }
