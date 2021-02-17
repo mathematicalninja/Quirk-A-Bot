eccb5d487df35cc6e8df8ad90d7017b02c4eb77a
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-17 11:46:01 +0000

    added eslint to highlight issues

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['airbnb-base'],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {
+		// for rules, 0 means ignore, 1 means warn, and 2 means error
+		'linebreak-style': 0,
+		indent: 0,
+		'no-tabs': 0,
+		radix: 0,
+		'no-plusplus': 0,
+		'spaced-comment': 0,
+		'no-console': 0,
+		'max-len': 0,
+		quotes: 0,
+		'no-multiple-empty-lines': 0,
+		'eol-last': 1
+	},
+};
