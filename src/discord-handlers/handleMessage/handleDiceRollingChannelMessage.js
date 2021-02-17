cbd81fcf740c9ff6c9a7d76c6892bb3db9bc72c7
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-17 16:22:37 +0000

    removed console.logs and added comments

--- src/discord-handlers/handleMessage/handleDiceRollingChannelMessage.js
+++ src/discord-handlers/handleMessage/handleDiceRollingChannelMessage.js
@@ -8,22 +8,21 @@ import { DICE_ROLL_REGEX, ROUSE_REGEX } from '../../constants.js';
  * @param {Object} msg Discord message object
  */
 export default function handleDiceRollingChannelMessage(msg) {
+	
+	// variable to hold regex result to prevent repeated executions
 	let regexMatch;
 
 	// handle dice roll
 	regexMatch = msg.content.match(DICE_ROLL_REGEX);
 	if (regexMatch) {
-		console.log('dice roll', { regexMatch });
 		// Add in a roll 3d10 style roller (with multi support "roll 3d4, 2d6")
 		const playerDice = regexToDice(regexMatch);
 		return msg.reply(doTheRolling(playerDice));
 	}
 
 	// handle player asked to rouse the blood.
-	regexMatch = msg.content.match(ROUSE_REGEX);
-	console.log({ regexMatch });
+	regexMatch = msg.content.match(ROUSE_REGEX); 
 	if (regexMatch) {
-		console.log('rouse check', { regexMatch });
 		const rouseDice = parseInt(regexMatch[1]) || 1;
 		return msg.reply(rollRouse(rouseDice));
 	}
