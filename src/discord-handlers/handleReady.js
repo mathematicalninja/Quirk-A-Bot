64989789ad38614e261198acd7ee761ef54e6ebf
mathematicalninja <github@mathematics.ninja>
2021-02-17 20:20:02 +0000

    Better humanisation

--- src/discord-handlers/handleReady.js
+++ src/discord-handlers/handleReady.js
@@ -1,4 +1,4 @@
 /** Discord ready handler */
 export default function handleReady() {
-	console.log(__filename, '\n\tloged on ok'); //the typo is intentional.
+	console.log(__filename, '\n\tI\'ve loged on ok'); //the typo is intentional.
 }
