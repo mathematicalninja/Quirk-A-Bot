c577e276112b606c13db0e0f1a3e66f318736be2
Eli <13420359+eliasm307@users.noreply.github.com>
2021-02-17 17:05:17 +0000

    renamed doTheRolling to rollVampire for consistency with 'rollRouse' and tweaked rol message when no pool dice defined

--- src/utils/rollVampire.js
+++ src/utils/rollVampire.js
@@ -3,17 +3,15 @@ import rollDice from './rollDice.js';
 import showRolls from './showRolls.js';
 
 /**
- * The wrapper function to do the actuall rolling
+ * The wrapper function to do the actual rolling
  * @param {Array<number>} playerDice array of numbers from the user
  * @returns {string} Message tooutput
  */
-export default function doTheRolling(playerDice) {
+export default function rollVampire(playerDice) {
 	// make sure atleast pool dice were defined
-	if (!playerDice[0] && !playerDice[1]) return 'Please define number of pool dice to roll atleast.';
+	if (!playerDice[0] && !playerDice[1]) return 'Please define the number of pool dice to roll atleast (greater than 0).';
 
-	// TODO respec to "vampireRolling" when other rolls are implemented
 	const [cleanDiceCount, hungerDiceCount] = kindOfDice(playerDice);
-
 	const RESULTS = [rollDice(cleanDiceCount), rollDice(hungerDiceCount)];
 	const WHAT_TO_WRITE = showRolls(RESULTS);
 	return WHAT_TO_WRITE;
